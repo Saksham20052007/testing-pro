17317c4990da129bf1659a280bd6317d10779f4e
Saksham20052007 <74138256+Saksham20052007@users.noreply.github.com>
2021-03-23 14:07:19 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint=Matter.Constraint;
 var roofObject, bobObject1, bobObject2, bobObject3, bobObject4, bobObject5, rope1, rope2, rope3, rope4, rope5;
+var ground;
 
 function preload()
 {
@@ -17,12 +18,14 @@ function setup() {
 
 	engine = Engine.create();
 	world = engine.world;
+
+	ground = new Ground(700, 690, width, 5);
 	roofObject = new roof(670, 200, 350, 20)
 	bobObject1 = new bob(550, 500, 60);
 	bobObject2 = new bob(610, 500, 60);
 	bobObject3 = new bob(670, 500, 60);
 	bobObject4 = new bob(730, 500, 60);
-	bobObject5 = new bob(790, 500, 60);
+	bobObject5 = new bob(780, 500, 60);
 	rope1 = new rope(roofObject.body, bobObject1.body, 90, 0);
 	World.add(world, rope1);
 	rope2 = new rope(roofObject.body, bobObject2.body, 90, 0);
@@ -47,8 +50,11 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(255);
+  text(mouseX+", "+mouseY, mouseX, mouseY)
+
   
   drawSprites();
+
   roofObject.display();
   bobObject1.display(); 
   bobObject2.display();
@@ -56,16 +62,18 @@ function draw() {
   bobObject4.display();
   bobObject5.display();
   strokeWeight(3);
-  line(bobObject1.body.position.x, bobObject1.body.position.y, roofObject.body.position.x, roofObject.body.position.y)
-  line(bobObject2.body.position.x, bobObject2.body.position.y, roofObject.body.position.x, roofObject.body.position.y)
-  line(bobObject3.body.position.x, bobObject3.body.position.y, roofObject.body.position.x, roofObject.body.position.y)
-  line(bobObject4.body.position.x, bobObject4.body.position.y, roofObject.body.position.x, roofObject.body.position.y)
-  line(bobObject5.body.position.x, bobObject5.body.position.y, roofObject.body.position.x, roofObject.body.position.y)
+  line(bobObject1.body.position.x, bobObject1.body.position.y, 550, roofObject.body.position.y)
+  line(bobObject2.body.position.x, bobObject2.body.position.y, 610, roofObject.body.position.y)
+  line(bobObject3.body.position.x, bobObject3.body.position.y, 670, roofObject.body.position.y)
+  line(bobObject4.body.position.x, bobObject4.body.position.y, 730, roofObject.body.position.y)
+  line(bobObject5.body.position.x, bobObject5.body.position.y, 780, roofObject.body.position.y)
+
+  keyPressed();
 
 }
 function keyPressed(){
 	if(keyCode===32){
-		Matter.body.applyForce(bobObject1.body, bobObject1.body.position, {x:-1, y:-2});
+		Matter.body.applyForce(bobObject5.body, bobObject5.body.position, {x:100, y:8});
 	}
 }
 
